8dcd79904588e0714a06cf3fe3193777e36032ba
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-12-03 14:56:55 +0100

    test: get all companies

--- backend/src/companies/companies.controller.spec.ts
+++ backend/src/companies/companies.controller.spec.ts
@@ -1,18 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CompaniesController } from './companies.controller';
+import { CompaniesService } from './companies.service';
 
 describe('CompaniesController', () => {
   let controller: CompaniesController;
+  const mockCompanies = [
+    {
+      id: 1,
+      name: 'Mayer and Sons',
+      country: 'Sweden',
+      foundingDate: new Date('2021-06-11T02:09:34Z'),
+      description: 'Secured scalable standardization',
+    },
+    {
+      id: 2,
+      name: 'Bartoletti and Sons',
+      country: null,
+      foundingDate: null,
+      description: 'Sharable contextually-based instruction set',
+    },
+  ];
+  const mockCompaniesService = {
+    findAll: jest.fn(() => Promise.resolve(mockCompanies)),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CompaniesController],
-    }).compile();
+      providers: [CompaniesService],
+    })
+      .overrideProvider(CompaniesService)
+      .useValue(mockCompaniesService)
+      .compile();
 
     controller = module.get<CompaniesController>(CompaniesController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  it('should return an array of companies', async () => {
+    const res = await controller.findAll();
+    expect(res).toEqual({ status: 200, body: mockCompanies });
   });
 });
--- backend/src/companies/companies.service.spec.ts
+++ backend/src/companies/companies.service.spec.ts
@@ -1,12 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CompaniesService } from './companies.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Company } from './companies.entity';
 
 describe('CompaniesService', () => {
   let service: CompaniesService;
+  const mockCompaniesRepo = {
+    find: jest.fn(() =>
+      Promise.resolve([
+        {
+          id: 1,
+          name: 'Mayer and Sons',
+          country: 'Sweden',
+          foundingDate: new Date('2021-06-11T02:09:34Z'),
+          description: 'Secured scalable standardization',
+        },
+        {
+          id: 2,
+          name: 'Bartoletti and Sons',
+          country: null,
+          foundingDate: null,
+          description: 'Sharable contextually-based instruction set',
+        },
+      ]),
+    ),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CompaniesService],
+      providers: [
+        CompaniesService,
+        {
+          provide: getRepositoryToken(Company),
+          useValue: mockCompaniesRepo,
+        },
+      ],
     }).compile();
 
     service = module.get<CompaniesService>(CompaniesService);
@@ -14,5 +42,10 @@ describe('CompaniesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return an array of companies with length 2', async () => {
+    const companies = await service.findAll();
+    expect(companies).toHaveLength(2);
   });
 });
