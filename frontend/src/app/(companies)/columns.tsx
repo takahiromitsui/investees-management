634325d0f7fe8dc430ff6d9500611ae38f8931e7
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-12-04 18:01:11 +0100

    feat: add pagination and view

--- frontend/src/app/(companies)/columns.tsx
+++ frontend/src/app/(companies)/columns.tsx
@@ -1,6 +1,16 @@
 'use client';
 import { format } from 'date-fns';
 import { ColumnDef } from '@tanstack/react-table';
+import { MoreHorizontal } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
 export type Deal = {
 	id: number;
@@ -45,6 +55,26 @@ export const columns: ColumnDef<Company>[] = [
 		cell: ({ row }) => {
 			const foundingDate = row.getValue('foundingDate') as string;
 			return format(new Date(foundingDate), 'dd/MM/yyyy');
+		},
+	},
+	{
+		id: 'actions',
+		cell: () => {
+			return (
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant='ghost' className='h-8 w-8 p-0'>
+							<span className='sr-only'>Open menu</span>
+							<MoreHorizontal className='h-4 w-4' />
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align='end'>
+						<DropdownMenuLabel>Actions</DropdownMenuLabel>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem>View deals</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			);
 		},
 	},
 ];
