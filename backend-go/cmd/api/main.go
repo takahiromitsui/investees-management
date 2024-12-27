957ed7d5dd4bff8e5be808e5d79d53956cfa935f
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-12-27 23:01:16 +0100

    feat:add session

--- backend-go/cmd/api/main.go
+++ backend-go/cmd/api/main.go
@@ -3,22 +3,39 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/takahiromitsui/investees-management/pkg/config"
 	"github.com/takahiromitsui/investees-management/pkg/handlers"
 	"github.com/takahiromitsui/investees-management/pkg/routes"
 )
 
+var appConfig config.AppConfig
+var session *scs.SessionManager
 
 func main() {
-	var appConfig config.AppConfig
+	appConfig.InProduction = false
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = appConfig.InProduction
+
+	appConfig.Session = session
+
 	repo := handlers.NewRepo(&appConfig)
 	handlers.SetRepository(repo)
 
 	mux := http.NewServeMux()
 	routes.RegisterRoutes(mux)
-	
-	if err := http.ListenAndServe("localhost:8080", mux); err != nil {
+	var port = "localhost:8080"
+	fmt.Printf("Starting server on port %s\n", port)
+
+	if err := http.ListenAndServe(
+		port,
+		mux,
+	); err != nil {
 		fmt.Println(err.Error())
 	}
 }
--- backend-go/go.mod
+++ backend-go/go.mod
@@ -1,3 +1,5 @@
 module github.com/takahiromitsui/investees-management
 
 go 1.22.3
+
+require github.com/alexedwards/scs/v2 v2.8.0 // indirect
