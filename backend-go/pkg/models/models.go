14a6a9e367503fba86d08a8cdad5f3d68cbf553a
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-12-30 10:29:24 +0100

    feat: create models

---
+++ backend-go/pkg/models/models.go
@@ -0,0 +1,31 @@
+package models
+
+import "time"
+
+// Define the Company struct
+type Company struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Country string `json:"country"`
+	FoundingDate time.Time `json:"foundingDate"`
+	Description string `json:"description"`
+	Deals []Deal `json:"deals"`
+}
+
+// Define the Deal struct
+type Deal struct {
+	ID   int    `json:"id"`
+	Date time.Time `json:"date"`
+	FundingAmount int `json:"fundingAmount"`
+	FundingRound string `json:"fundingRound"`
+	CompanyID int `json:"companyID"`
+}
+
+// Define the User struct
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Email string `json:"email"`
+	Password string `json:"password"`
+	RefreshToken string `json:"refreshToken"`
+}
